13424b99ceadc34d8fa8e96e9161bc83e2c28845
Hans <dornsback@gmail.com>
2025-04-12 14:25:55 -0300

    quizv1

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 )
 
-func main() {
-	fmt.Println("hello go")
+type Quiz struct {
+	Question string
+	Answer   string
 }
+
+func main() {
+	fmt.Println("Loading CSV file... 🐼")
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	data, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Done!")
+
+	questions := make([]Quiz, len(data))
+
+	for i, data := range data {
+		question := Quiz{
+			Question: data[0],
+			Answer:   data[1],
+		}
+		questions[i] = question
+	}
+
+	for i, questions := range questions {
+		fmt.Printf("Question %v: %v\n", i, questions.Question)
+	}
+	//fmt.Println(questions)
+
+}
+
+// func loadCsv()
